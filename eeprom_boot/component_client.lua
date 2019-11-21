5a30055df32df4db229dd3393662de4576c6be72
hohserg <hohserg1@yandex.ru>
2019-11-21 22:36:14 +0300

    simplify event pull

--- eeprom_boot/component_client.lua
+++ eeprom_boot/component_client.lua
@@ -193,7 +193,6 @@ end
 --
 
 --serialization
-
 function unserialize(data)
   checkArg(1, data, "string")
   local result, reason = load("return " .. data, "=data", nil, {math={huge=math.huge}})
@@ -228,58 +227,22 @@ end
 --
 
 --event
-local function pullFiltered(...)
-  local args = table.pack(...)
-  local seconds, filter = math.huge
-
-  if type(args[1]) == "function" then
-    filter = args[1]
-  else
-    checkArg(1, args[1], "number", "nil")
-    checkArg(2, args[2], "function", "nil")
-    seconds = args[1]
-    filter = args[2]
-  end
-
-  repeat
-    local signal = table.pack(computer.pullSignal(seconds))
-    if signal.n > 0 then
-      if not (seconds or filter) or filter == nil or filter(table.unpack(signal, 1, signal.n)) then
-        return table.unpack(signal, 1, signal.n)
-      end
-    end
-  until signal.n == 0
+local function pullFiltered(filter)
+	local signal = table.pack(computer.pullSignal(math.huge))
+	if filter(table.unpack(signal, 1, signal.n)) then
+		return table.unpack(signal, 1, signal.n)
+	end
 end
 
-local function createPlainFilter(name, ...)
-  local filter = table.pack(...)
-  if name == nil and filter.n == 0 then
-    return nil
-  end
-
+local function createPlainFilter(name)
   return function(...)
     local signal = table.pack(...)
-    if name and not (type(signal[1]) == "string" and signal[1]:match(name)) then
-      return false
-    end
-    for i = 1, filter.n do
-      if filter[i] ~= nil and filter[i] ~= signal[i + 1] then
-        return false
-      end
-    end
-    return true
+    return (type(signal[1]) == "string" and signal[1]:match(name))
   end
 end
 
-local function pull(...)
-  local args = table.pack(...)
-  if type(args[1]) == "string" then
-    return pullFiltered(createPlainFilter(...))
-  else
-    checkArg(1, args[1], "number", "nil")
-    checkArg(2, args[2], "string", "nil")
-    return pullFiltered(args[1], createPlainFilter(select(2, ...)))
-  end
+local function pull(name)
+	return pullFiltered(createPlainFilter(name))
 end
 --
 
