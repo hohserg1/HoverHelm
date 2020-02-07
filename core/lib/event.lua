a082495bcc1e509f239c432772c3adb1add69e98
hohserg <hohserg1@yandex.ru>
2020-02-07 17:13:19 +0300

    Implement even library
    event.pull - filtered signal pull
    event.listen - register event handlers, may be many for one event
    computer.pullSignal - appling event handlers

--- core/lib/event.lua
+++ core/lib/event.lua
@@ -3,18 +3,42 @@ local event={}
 local handlers={}
 
 function event.listen(event_name,handler)
-    handlers[event_name]=handler
+    handlers[event_name]=handlers[event_name] or {}
+    table.insert(handlers[event_name],handler)
 end
 
-function event.pull(event_name)
+local opullSignal=computer.pullSignal
+
+computer.pullSignal=function(timeout)
+    local signal=table.pack(opullSignal(timeout))
+
+    local h=handlers[signal[1]]
+
+    if h and #h>0 then
+        for _, value in pairs(h) do
+            pcall(value,table.unpack(signal))
+        end
+    end
+
+    return table.unpack(signal)
+end
+
+function event.pull(...)
+    local args=table.pack(...)
+    local timeout, event_name
+    if type(args[1])=="number" then
+        timeout, event_name = args[1],args[2]
+    else
+        timeout, event_name = math.huge,args[1]
+    end
+
+    local start=computer.uptime()
+
     local signal
     repeat
-        signal=table.pack(computer.pullSignal(math.huge))
-        local h=handlers[signal[1]]
-        if h then
-            h(table.unpack(signal))
-        end
-    until signal[1]==event_name
+        signal=table.pack(opullSignal(timeout))
+    until not event_name or signal[1]==event_name or computer.uptime()-start>timeout
+
     return table.unpack(signal)
 end
 
