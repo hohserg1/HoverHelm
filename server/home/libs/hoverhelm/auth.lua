b2f7e3ae24352e44b1f820540cce08b18eb4d127
hohserg <hohserg1@yandex.ru>
2020-08-13 01:56:52 +0300

    Added log print for auth

--- server/home/libs/hoverhelm/auth.lua
+++ server/home/libs/hoverhelm/auth.lua
@@ -1,11 +1,14 @@
 return {
     handlers={
-        hh_connect = function(card,sender,deviceName)
-            local actualDeviceName = userdata.getOrRegisterDeviceName(sender,deviceName)
+        hh_connect = function(card, sender, deviceName)
+            local actualDeviceName = userdata.getOrRegisterDeviceName(sender, deviceName)
+            terminal.noticeLocalLog(terminal.log_level.msg, sender.." try to connect with name "..deviceName)
             if actualDeviceName then
-                card.send(sender,"hh_ok",actualDeviceName)            
+                card.send(sender,"hh_ok", actualDeviceName)
+                terminal.noticeLocalLog(terminal.log_level.msg, sender.."\\"..actualDeviceName.." successfuly connected")       
             else
-                card.send(sender,"hh_error","name is alredy busy")            
+                card.send(sender,"hh_error", "name is alredy busy")     
+                terminal.noticeLocalLog(terminal.log_level.error, sender.." failure connected: name is alredy busy")        
             end
         end
     }
