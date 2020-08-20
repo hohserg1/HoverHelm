663de756450e88c61f78ed9a5344a07cbb1bd774
hohserg <hohserg1@yandex.ru>
2020-08-20 20:47:50 +0300

    Rename /libs -> /lib

--- server/home/lib/hoverhelm/config.lua
+++ server/home/lib/hoverhelm/config.lua
@@ -2,8 +2,8 @@ return {
     userRootFolder="/home/hoverhelm/devices/",
     coreRootFolder="/home/hoverhelm/device_core/",
     inUseNetworkCards = {
-        ["address"] = {
-            port = 1 -- 0 for tunnel
+        ["351625f3-fc85-4d75-abc1-ca77a7619101"] = {
+            port = 0 -- 0 for tunnel
         }
     },
     timezone=0,
