f7f9f9a7020a40aa0a07b5d4bad42fa89d7f9a5a
hohserg <hohserg1@yandex.ru>
2020-08-23 19:31:08 +0300

    Added prepare_eeprom server command

--- server/home/lib/hoverhelm/commands.lua
+++ server/home/lib/hoverhelm/commands.lua
@@ -1,3 +1,35 @@
+local component=require"component"
+local filesystem=require"filesystem"
+
+local function compress(content)
+    return lzss.getSXF(lzss.compress(content), true)
+end
+
+local function prepareContent(content, networkCardType)
+    return content:gsub("%-%-%-%[%[%#if_def "..networkCardType, ""):gsub("%-%-%-%[%[%#if_def ","--[[")
+end
+
+local function loadPrimaryBIOS(networkCardType)
+    local location = "/home/hoverhelm/eeprom-client/"
+    local fileName = location.."primary-eeprom-"..networkCardType..".lua"
+    if filesystem.exists(fileName) then
+        local h = io.open(fileName, "r")
+        local content = h:read("*a")
+        h:close()
+        return content
+    else
+        local h = io.open(location.."primary-eeprom.lua", "r")
+        local content = compress(prepareContent(h:read("*a"), networkCardType))
+        h:close()
+        
+        local h1 = io.open(fileName, "w")
+        h1:write(content)
+        h1:close()
+        
+        return content
+    end
+end
+
 return {
     hide = function()
         hoverhelm.hide()
@@ -15,5 +47,19 @@ return {
                                           "Thx to swg2you or bibi.lua program - useful for testing \n"..
                                           "Thx to everyone who uses it or gives any help \n"
         )
+    end,
+    prepare_eeprom  = function(newDeviceName, serverNetWorkCard, port, clientNetWorkCard)
+        serverNetWorkCard = component.list(serverNetWorkCard)() or component.get(serverNetWorkCard)
+        if userdata.getDeviceAddress(newDeviceName) then
+            terminal.noticeLocalLog(terminal.log_level.error, "name", newDeviceName, "is alredy busy")
+        elseif not config.inUseNetworkCards[serverNetWorkCard] then
+            terminal.noticeLocalLog(terminal.log_level.error, "network card", serverNetWorkCard, "is not configured. need to add it to config")
+        else
+            local eeprom = component.eeprom
+            eeprom.set(loadPrimaryBIOS(component.type(serverNetWorkCard)))
+            eeprom.setLabel(newDeviceName)
+            eeprom.setData(serverNetWorkCard..":"..tostring(port or 0)..":"..(clientNetWorkCard or ""))
+            terminal.noticeLocalLog(terminal.log_level.msg, "successful to create new device eeprom!")
+        end
     end
 }
